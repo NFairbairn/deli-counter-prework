d030c2f057664fade8841da017e0771826e81f67
Noah Fairbairn <noahfairbairn@gmail.com>
2019-01-29 18:09:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,3 +12,19 @@ def take_a_number (katz_deli, name)
   katz_deli.push(name)
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
+
+def now_serving (katz_deli, name)
+name = katz_deli.shift
+  if katz_deli.length > 0
+    puts "Currently serving #{name}."
+  else
+    puts "There is nobody waiting to be served"
+  end
+end
+
+
+
+
+
+
+    
